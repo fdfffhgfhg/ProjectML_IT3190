d7a5b7362ae2fac9fd0d83d4e9ccc60f080cf77d
TuanTaQuoc <61730601+fdfffhgfhg@users.noreply.github.com>
2025-05-25 23:24:18 +0700

    Update model.py
    Signed-off-by: TuanTaQuoc <61730601+fdfffhgfhg@users.noreply.github.com>

--- Source/model.py
+++ Source/model.py
@@ -8,7 +8,7 @@ def train_model(input_dim, output_dim, activation="leaky_relu", dropout=0.2):
     
     inputs = layers.Input(shape=input_dim, name="input")
 
-    # normalize images here instead in preprocessing step
+    #noremalize image
     input = layers.Lambda(lambda x: x / 255)(inputs)
 
     x1 = residual_block(input, 16, activation=activation, skip_conv=True, strides=1, dropout=dropout)
